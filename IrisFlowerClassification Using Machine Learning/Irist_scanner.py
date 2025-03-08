3df7f08263724bc0395fed3f2e644a2280e08c1b
Akhil Singh <150819607+GSAPify@users.noreply.github.com>
2025-03-09 03:08:08 +0530

    Update Irist_scanner.py

--- IrisFlowerClassification Using Machine Learning/Irist_scanner.py
+++ IrisFlowerClassification Using Machine Learning/Irist_scanner.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
 from numpy.conftest import dtype
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
+model = LogisticRegression()
 warnings.simplefilter("ignore")
 
 
@@ -42,13 +45,6 @@ sns.heatmap(df.drop('Species', axis=1).corr(),annot=True,cmap='coolwarm')
 plt.title("Correlation Matrix")
 plt.show()
 
-#Box Plot for Outlier
-for col in ['SepalLength', 'SepalWidth','PetalLength','PetalWidth']:
-    sns.boxplot(df[col])
-    plt.title(f'Box plot of {col}')
-    plt.show()
-
-
 #Define x and y
 
 x=df[['SepalLength', 'SepalWidth','PetalWidth','PetalLength']] #Independent Variables
@@ -66,73 +62,29 @@ print("Y_train shape:", y_train.shape)
 print("Y_test shape:", y_test.shape)
 
 
-print(df.describe())
-from email.errors import FirstHeaderLineIsContinuationDefect
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import warnings
-from numpy.conftest import dtype
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
 
-warnings.simplefilter("ignore")
+#calculate accuracy and confusion matrix
+accuracy = accuracy_score(y_test,y_pred)*100
+conf_matrix = confusion_matrix(y_test,y_pred)
 
 
-#Import iris Dataset.
-df=pd.read_csv(r'C:\Users\PC\Downloads\iris\iris.data')
+print("Accuracy of the model is {:.2f}%".format(accuracy))
+print("Confusion Matrix: ")
+print(conf_matrix)
 
+#print Classification report
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
 
-# Assign column names to the dataset
-df.columns = ['SepalLength', 'SepalWidth', 'PetalLength','PetalWidth', 'Species']
+#visualize confusion report
 
-# Dataset Info
-df.info()
-
-# Checking for null values
-print(df.isnull().sum())
-
-# Display column names
-print(df.columns)
-
-#Value counts for Species column
-print("Unique Species:", df['Species'].unique())
-print("Duplicate Rows:", df.duplicated().sum())
-
-#Visualize the species count
-sns.pairplot(df,hue='Species', palette='husl')
-sns.set(style="whitegrid") #seaborn style
-plt.title("Pair Plot of Iris DataSet")
+plt.figure(figsize=(8,6))
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=model.classes_,yticklabels=model.classes_)
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Lables')
+plt.title('Confusion Matrix')
 plt.show()
-
-
-#Heatmaps
-plt.figure(figsize=(10,6))
-sns.heatmap(df.drop('Species', axis=1).corr(),annot=True,cmap='coolwarm')
-plt.title("Correlation Matrix")
-plt.show()
-
-#Box Plot for Outlier
-for col in ['SepalLength', 'SepalWidth','PetalLength','PetalWidth']:
-    sns.boxplot(df[col])
-    plt.title(f'Box plot of {col}')
-    plt.show()
-
-
-#Define x and y
-
-x=df[['SepalLength', 'SepalWidth','PetalWidth','PetalLength']] #Independent Variables
-y=df['Species'] #Dependent Variable
-
-# Split the dataset into training and testing sets
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test=train_test_split (x,y,random_state=0, test_size=0.2) #20% Test Set
-
-#Check Shapes
-
-print("X_train shape:", x_train.shape)
-print("X_test shape:", x_test.shape)
-print("Y_train shape:", y_train.shape)
-print("Y_test shape:", y_test.shape)
-
 
 print(df.describe())
